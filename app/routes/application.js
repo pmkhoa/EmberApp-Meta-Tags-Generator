d9819e975da3f2ccbf56dd0e2604b341b0cb73eb
Khoa Pham <khoa.pham@me.com>
2016-04-20 22:58:00 -0700

    Generate meta models & add sample data to template

---
+++ app/routes/application.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+    model() {
+        return Ember.RSVP.hash({
+            standardTags: this.store.createRecord('standard-meta'),
+            facebookTags: this.store.createRecord('facebook-meta'),
+            twitterTags: this.store.createRecord('twitter-meta'),
+            googleTags: this.store.createRecord('google-plus-meta'),
+        });
+    },
+
+    setupController(controller, models) {
+        controller.set('standardTags', models.standardTags);
+        controller.set('facebookTags', models.facebookTags);
+        controller.set('twitterTags', models.twitterTags);
+        controller.set('googleTags', models.googleTags);
+    }
+});
